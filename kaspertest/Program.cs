ee0e3be95e1a5954e51f0f7df29ae6fbb8447a01
acidnaya <acidnaya@gmail.com>
2022-10-14 16:39:00 +0300

    ...

---
+++ kaspertest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace kaspertest
+{
+    class Program
+    {
+        // Так как в условии сказано, что все команды точно соответствуют формату,
+        // я проверять на корректность переход в дочернюю папку никак не буду,
+        // методом исключения это команда такого типа.
+        public static int Operate(List<string> ops)
+        {
+            int distance = 0;
+            foreach (var op in ops)
+            {
+                switch (op)
+                {
+                    case "../" :
+                        if (distance != 0)
+                            distance--;
+                        break;
+                    case "./" :
+                        break;
+                    default:
+                        distance++;
+                        break;
+                }
+            }
+            return distance;
+        }
+        
+        static void Main(string[] args)
+        {
+            List<string> ops = Console.ReadLine()?.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+            Console.WriteLine(Operate(ops));
+        }
+    }
+}
